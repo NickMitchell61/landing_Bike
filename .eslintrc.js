25ac4cbb94078d9b7760d8c2af1510775ef18a07
Maksym Nemera <nickmitchell@ukr.net>
2023-06-30 15:04:06 +0300

    add full landing page

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
-  extends: "@mate-academy/eslint-config",
+  extends: '@mate-academy/eslint-config',
 
-  rules: {
-    "linthtml/tag-self-close": "on",
+  "parserOptions": {
+    "sourceType": "module"
   },
 };
